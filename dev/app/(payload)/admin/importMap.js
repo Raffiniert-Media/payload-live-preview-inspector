7c7bc6d82ea522987920349bb3fd5315902f894f
Florian Hitz <florian@raffiniert.biz>
2026-07-13 13:03:21 +0200

    feat: initial commit

---
+++ dev/app/(payload)/admin/importMap.js
@@ -0,0 +1,9 @@
+import { BeforeDashboardClient as BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343 } from 'payload-live-preview-inspector/client'
+import { BeforeDashboardServer as BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f } from 'payload-live-preview-inspector/rsc'
+
+export const importMap = {
+  'payload-live-preview-inspector/client#BeforeDashboardClient':
+    BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343,
+  'payload-live-preview-inspector/rsc#BeforeDashboardServer':
+    BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f,
+}
